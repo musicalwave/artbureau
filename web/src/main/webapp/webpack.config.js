965e112b214651c76e42b09839919f4f0386c564
razdvapoka <razdvapoka@gmail.com>
2016-04-01 16:55:53 +0300

    Enter the webpack!

---
+++ web/src/main/webapp/webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    client: './src/client.js',
+    schedule: './src/schedule.js'
+  },
+  output: {
+    filename: '[name].min.js',
+    path: __dirname + '/resources/js'
+  },
+  module: {
+    loaders: [{ 
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true, 
+      compress: {
+        warnings: false
+      }})
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+};
